49b47ba3a40f0183ec87e922d384bf6e6c51cd3c
sklibad <sklibad@natur.cuni.cz>
2025-02-07 22:51:58 +0100

    Adding more of the current progress

---
+++ spatial_data.js
@@ -0,0 +1,14 @@
+var pointLayer = {
+    "type": "FeatureCollection",
+    "name": "svatba",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": 1, "typ": "svatba", "url": "https://www.spielberg.cz/" }, "geometry": { "type": "Point", "coordinates": [ 16.998763606634981, 49.043953964619071 ] } },
+    { "type": "Feature", "properties": { "id": 2, "typ": "hotel", "url": "https://www.hotelausterlitz.cz/cs/" }, "geometry": { "type": "Point", "coordinates": [ 16.871992348395381, 49.157234452327195 ] } },
+    { "type": "Feature", "properties": { "id": 3, "typ": "hotel", "url": "https://www.rustico.cz/" }, "geometry": { "type": "Point", "coordinates": [ 17.001100815430906, 49.151958393243632 ] } },
+    { "type": "Feature", "properties": { "id": 4, "typ": "hotel", "url": "https://www.kyjovpenzion.cz/" }, "geometry": { "type": "Point", "coordinates": [ 17.124897475478001, 49.010013588769823 ] } },
+    { "type": "Feature", "properties": { "id": 5, "typ": "hotel", "url": "https://www.apartmanykyjov.cz/" }, "geometry": { "type": "Point", "coordinates": [ 17.130797549198874, 49.007618202502172 ] } },
+    { "type": "Feature", "properties": { "id": 6, "typ": "hotel", "url": "https://www.balaba.cz/" }, "geometry": { "type": "Point", "coordinates": [ 16.895807263463464, 48.956043149447432 ] } }
+    ]
+    }
+    ;    
